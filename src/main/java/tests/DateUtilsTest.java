2b104153821623bbfbba3e853f2d09c1b609b04f
Roni Capela Pereira <ronicapela@Mac-mini-de-Roni.local>
2024-01-19 14:58:47 -0300

    test(): testes unitários

---
+++ src/main/java/tests/DateUtilsTest.java
@@ -0,0 +1,28 @@
+package tests;
+
+import br.ce.wcaquino.taskbackend.utils.DateUtils;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+
+public class DateUtilsTest {
+
+    @Test
+    public void deveRetornarTrueParaDatasFuturas() {
+        LocalDate date = LocalDate.of(2030, 1, 1);
+        Assertions.assertTrue(DateUtils.isEqualOrFutureDate(date));
+    }
+
+    @Test
+    public void deveRetornarFalseParaDatasPassadas() {
+        LocalDate date = LocalDate.of(2010, 1, 1);
+        Assertions.assertFalse(DateUtils.isEqualOrFutureDate(date));
+    }
+
+    @Test
+    public void deveRetornarTrueParaDataAtual() {
+        LocalDate date = LocalDate.now();
+        Assertions.assertTrue(DateUtils.isEqualOrFutureDate(date));
+    }
+}
---
+++ src/main/java/tests/TaskControllerTest.java
@@ -0,0 +1,77 @@
+package tests;
+
+import br.ce.wcaquino.taskbackend.controller.TaskController;
+import br.ce.wcaquino.taskbackend.model.Task;
+import br.ce.wcaquino.taskbackend.repo.TaskRepo;
+import br.ce.wcaquino.taskbackend.utils.ValidationException;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+
+import java.time.LocalDate;
+
+public class TaskControllerTest {
+
+    @Mock
+    private TaskRepo taskRepo;
+
+    @InjectMocks
+    private TaskController controller;
+
+    @BeforeEach
+    public void setup() {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    public void naoDeveSalvarTarefaSemDescricao() {
+        Task todo = new Task();
+        todo.setDueDate(LocalDate.now());
+//        todo.setTask("Descricao");
+        try {
+            controller.save(todo);
+            Assertions.fail("Não deveria chegar nesse ponto!");
+        } catch (ValidationException e) {
+            Assertions.assertEquals("Fill the task description", e.getMessage());
+        }
+    }
+
+    @Test
+    public void naoDeveSalvarTarefaSemData() {
+        Task todo = new Task();
+//        todo.setDueDate(LocalDate.now());
+        todo.setTask("Descricao");
+        try {
+            controller.save(todo);
+            Assertions.fail("Não deveria chegar nesse ponto!");
+        } catch (ValidationException e) {
+            Assertions.assertEquals("Fill the due date", e.getMessage());
+        }
+    }
+
+    @Test
+    public void naoDeveSalvarTarefaComDataPassada() {
+        Task todo = new Task();
+        todo.setDueDate(LocalDate.of(2010, 1, 1));
+        todo.setTask("Descricao");
+        try {
+            controller.save(todo);
+            Assertions.fail("Não deveria chegar nesse ponto!");
+        } catch (ValidationException e) {
+            Assertions.assertEquals("Due date must not be in past", e.getMessage());
+        }
+    }
+
+    @Test
+    public void deveSalvarTarefaComSucesso() throws ValidationException {
+        Task todo = new Task();
+        todo.setDueDate(LocalDate.now());
+        todo.setTask("Descricao");
+        controller.save(todo);
+        Mockito.verify(taskRepo).save(todo);
+    }
+}
